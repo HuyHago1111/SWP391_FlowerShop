86e10fe6a6b15f76aeac6ecc8cc284541bf8151e
Nguyen Dat <0915540515x@gmail.com>
2025-10-11 00:36:44 +0700

    Initialize FlowerShop project with basic structure and configuration files

---
+++ src/main/java/com/flowerShop1/service/product/ProductService.java
@@ -0,0 +1,9 @@
+package com.flowerShop1.service.product;
+
+import com.flowerShop1.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAllProduct();
+}
