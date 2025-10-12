6ad26a93d7f324a63aebba4c2c938661abe1b8e0
Nguyen Dat <0915540515x@gmail.com>
2025-10-13 02:05:49 +0700

    Initialize FlowerShop project with basic structure and configuration files

--- src/main/java/com/flowerShop1/repository/ProductsRepository.java
+++ src/main/java/com/flowerShop1/repository/ProductsRepository.java
@@ -4,4 +4,5 @@ import com.flowerShop1.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductsRepository  extends JpaRepository <Product, Integer> {
+    Product findByProductId(int productId);
 }
--- src/main/java/com/flowerShop1/service/user/impl/UserService/UserServiceimpl.java
+++ src/main/java/com/flowerShop1/service/user/impl/UserService/UserServiceimpl.java
@@ -1,6 +1,9 @@
 package com.flowerShop1.service.user.impl.UserService;
 
+import com.flowerShop1.dto.user.UserSignUpDTO;
 import com.flowerShop1.entity.User;
+import com.flowerShop1.mapper.user.UserMapper;
+import com.flowerShop1.mapper.user.UserSignUpMapper;
 import com.flowerShop1.repository.UserRepository;
 import com.flowerShop1.service.mail.MailService;
 import com.flowerShop1.service.user.UserService;
@@ -14,6 +17,10 @@ public class UserServiceimpl implements UserService {
     private UserRepository userRepository;
     @Autowired
     private MailService mailService;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private UserSignUpMapper userSignUpMapper;
 
     @Override
     public List<User> getAllUsers() {
@@ -21,13 +28,27 @@ public class UserServiceimpl implements UserService {
     }
 
     @Override
-    public void register(User user) {
+    public void register(UserSignUpDTO user) {
         if (userRepository.existsByEmail(user.getEmail())) {
             throw new IllegalArgumentException("Email already exists");
-        } else {
-            userRepository.save(user);
         }
+        UserSignUpDTO dto = new UserSignUpDTO();
+
+        String otp = generateOTP();
+        user.setOtp(otp);
+        user.setOtpExprirationTime(java.time.LocalDateTime.now().plusMinutes(3));
+
+        mailService.sendOTP(user.getEmail(), otp);
         //hàm random otp 6 số ngẫu nhiên
+
+    }
+    @Override
+    public boolean verifyOTP(String otp, UserSignUpDTO userSignUpDTO){
+        if (userSignUpDTO.getOtp().equals(otp) && userSignUpDTO.getOtpExprirationTime().isAfter(java.time.LocalDateTime.now())){
+            userRepository.save(userSignUpMapper.dtoToEntity(userSignUpDTO));
+            return true;
+        }
+        return false;
 
     }
     public String generateOTP() {
