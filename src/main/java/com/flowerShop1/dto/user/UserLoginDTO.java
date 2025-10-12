4ef922f73ff74668ea41ef34568d6926156729e5
Nguyen Dat <0915540515x@gmail.com>
2025-10-12 11:51:12 +0700

    Initialize FlowerShop project with basic structure and configuration files

---
+++ src/main/java/com/flowerShop1/dto/user/UserLoginDTO.java
@@ -0,0 +1,23 @@
+package com.flowerShop1.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class UserLoginDTO {
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
--- src/main/java/com/flowerShop1/repository/UserRepository.java
+++ src/main/java/com/flowerShop1/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository <User, Integer> {
+    User findByEmail(String email);
+
 
 }
