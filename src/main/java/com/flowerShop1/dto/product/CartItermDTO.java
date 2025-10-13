b7efe37047f467efe235ade80e7215c2e097ebd6
Nguyen Dat <0915540515x@gmail.com>
2025-10-13 23:51:41 +0700

    Add trending products feature and fix minor issues in product display

---
+++ src/main/java/com/flowerShop1/dto/product/CartItermDTO.java
@@ -0,0 +1,19 @@
+package com.flowerShop1.dto.product;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CartItermDTO {
+    private int productId;
+    private String productName;
+    private String productImage;
+    private double productPrice;
+    private int StockQuantity;
+    private double totalPrice;
+    private int quantityCart;
+
+}
